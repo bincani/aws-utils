83ad4b1ebc60474176f63219a1620b00ddf26d76
Ben Incani <ben.incani@factoryx.com.au>
2019-09-16 09:13:17 +1000

    update access

--- powerShell/Microsoft.PowerShell_profile.ps1
+++ powerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,13 @@
 # use for testing
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 
-# confi aws
-[string]$awsProfile = "profile"
-[string]$awsRegion = "region"
-[string]$awsSecurityGroup = "security-group"
-
 Import-Module AWSPowerShell
 
-Set-AWSCredential -ProfileName $awsProfile
+. "$PSScriptRoot\_Revoke-ALL-Access.ps1"
+. "$PSScriptRoot\_Grant-SSH-Access.ps1"
+
+# config aws
+[string]$awsRegion = "ap-southeast-2"
 Set-DefaultAWSRegion -Region $awsRegion
 
 $host.UI.RawUI.WindowTitle = "AWS PS"
@@ -16,11 +15,25 @@ $host.UI.RawUI.WindowTitle = "AWS PS"
 # Welcome message
 "You are now entering PowerShell : " + $env:Username
 
-. "$PSScriptRoot\_Revoke-ALL-Access.ps1"
-Revoke-ALL-Access $awsSecurityGroup
+[string]$awsProfile = "binca-prod"
+[string]$awsSecurityGroup = "sg-86d348e3"
+Set-AWSCredential -ProfileName $awsProfile
 
-. "$PSScriptRoot\_Grant-SSH-Access.ps1"
-Grant-SSH-Access $awsSecurityGroup
+Revoke-ALL-Access $awsSecurityGroup
+Grant-SSH-Access $awsSecurityGroup 22
+Grant-SSH-Access $awsSecurityGroup 80
+Grant-SSH-Access $awsSecurityGroup 443
+Grant-SSH-Access $awsSecurityGroup 33402
+
+[string]$awsProfile = "binca-stage"
+[string]$awsSecurityGroup = "sg-4cfb6329"
+Set-AWSCredential -ProfileName $awsProfile
+
+Revoke-ALL-Access $awsSecurityGroup
+Grant-SSH-Access $awsSecurityGroup 22
+Grant-SSH-Access $awsSecurityGroup 80
+Grant-SSH-Access $awsSecurityGroup 443
+Grant-SSH-Access $awsSecurityGroup 33402
 
 # pause script (use for testing)
 #Read-Host -Prompt "Press Enter to continue"
